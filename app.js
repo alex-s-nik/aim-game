2e0d1dba59dbcc9cb4f10e1e9d08f9425765f4a0
alex-s-nik <to_ctrl@mail.ru>
2021-04-15 16:04:17 +0300

    feat: first version of the game

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-let radius = 2;
+// basic variables
+const radius = 2;
+let score = 0;
+let clicks = 0;
+const aimShowTime = 1301;
+const gameTime = 20000;
+let aimAppearTimer;
+let gameTimer;
+let timeLeftUpdate;
+let timesShow = 0;
+let isGameFinished = false;
+let aim = makeAim(radius);
+let aimPadding = 10;
+
+// element variables
 const gameField = document.querySelector(".game-field");
 const gamePanel = document.querySelector(".header__game-panel")
 const scoreField = document.querySelector(".score");
 const clicksField = document.querySelector(".clicks");
+const triesField = document.querySelector(".tries");
+const gameTimerField = document.querySelector(".timer");
 const startGameButton = document.querySelector(".start-game");
-let score = 0;
-let clicks = 0;
+const menu = document.querySelector(".menu");
+const results = document.querySelector(".results__text");
+
 
 function makeAim(radius) {
   const countCircles = 10;
@@ -24,7 +41,7 @@ function makeAim(radius) {
     parentElement.append(currentCircleElement);
     parentElement = currentCircleElement;
   }
-  rootElement.style.visibility = "hidden";
+  rootElement.style.display = "none";
   rootElement.style.position = "absolute";
   return rootElement;
 }
@@ -35,33 +52,65 @@ function clickOnAim(e) {
   score += parseInt(elementScore);
   scoreField.textContent = score;
   clicksField.textContent = ++clicks;
-  showAim();
+  hideAim();
 }
 
 function showAim() {
-  let aimTop = Math.round(Math.random() * (aim.parentElement.clientHeight - aim.clientHeight));
-  let aimLeft = Math.round(Math.random() * (aim.parentElement.clientWidth - aim.clientWidth));
+  let aimTop = Math.round(Math.random() * (aim.parentElement.clientHeight - aim.clientHeight - 2 *aimPadding) + aimPadding); 
+  let aimLeft = Math.round(Math.random() * (aim.parentElement.clientWidth - aim.clientWidth - 2 * aimPadding) + aimPadding);
   aim.style.top = `${aimTop}px`;
   aim.style.left = `${aimLeft}px`;
-  aim.style.visibility = "";
+  aim.style.display = "block";
+  triesField.textContent = (++timesShow);
 }
 
 function hideAim() {
-  aim.style.visibility = "hidden";
+  aim.style.display = "none";
 }
 
 function startGameButtonClick() {
-  startGameButton.style.display = "none";
+  menu.style.display = "none";
+  gameField.append(aim);
   gamePanel.style.display = "block";
+  aimAppearTimer = setInterval(showAim, aimShowTime);
+  gameTimer = setTimeout(gameOver, gameTime);
+
+  let gameOverTime = Date.now() + gameTime;
+  timeLeftUpdate = setInterval(()=>{
+
+    let distance = gameOverTime - Date.now();
+    let minutes = Math.round(distance / (1000 * 60));
+    console.log(`${minutes}`);
+   
+    let seconds = Math.round(distance / 1000) % 60;
+    console.log(`${seconds}`);
+    gameTimerField.textContent = `${minutes}:${seconds < 9 ? "0" : ""}${seconds}`;
+  
+  }, 500);
   showAim();
 }
 
-let aim = makeAim(radius);
+function gameOver () {
+  clearInterval(aimAppearTimer);
+  clearInterval(timeLeftUpdate);
+  aim.style.display = "none";
+  isGameFinished = true;
+  initGame();
+}
+
+// add listeners
 aim.addEventListener("click", clickOnAim);
 startGameButton.addEventListener("click", startGameButtonClick);
-gameField.append(aim);
 
-// TODO:
-// 1. Make end of the game
-// 2. Calc stats
-// 3. Timers
+
+function initGame() {
+  if (isGameFinished) {
+    results.textContent = `Score: ${score}, mistakes: ${timesShow - clicks}`;
+    startGameButton.textContent = "Play again!";
+  }
+  timesShow = 0;
+  score = 0;
+  clicks = 0;
+  menu.style.display = "block";
+  gamePanel.style.display = "none";
+}
